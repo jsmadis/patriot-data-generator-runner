dfe32d1639eaaa814f5f27f5c4a2fb37960c57ea
Jakub <jakub.smadis@gmail.com>
2021-05-03 23:02:15 +0200

    Refactoring and removing unused code

--- src/main/java/io/patriot_framework/generator/runner/Runner.java
+++ src/main/java/io/patriot_framework/generator/runner/Runner.java
@@ -1,28 +1,31 @@
 package io.patriot_framework.generator.runner;
 
-import io.patriot_framework.generator.controll.client.CoapControlClient;
 import io.patriot_framework.generator.controll.server.CoapControlServer;
 import io.patriot_framework.generator.device.Device;
+import io.patriot_framework.generator.device.active.ActiveDevice;
 import io.patriot_framework.generator.runner.configuration.Constants;
 import io.patriot_framework.generator.utils.JSONSerializer;
 
 import java.io.File;
 
 /**
- * @author <a href="mailto:jakub.smadis@gmail.com">Jakub Smadiš</a>
+ * @author <a href="mailto:jakub.smadis@gmail.com">Jakub Smadis</a>
  */
 public class Runner {
 
     public static void main(String[] args) {
-        String deviceFile = System.getenv(Constants.DEVICE_CONFIG_FILE);
-        Device device = JSONSerializer.deserializeDevice(new File(deviceFile));
-        device.registerToCoapServer();
-        CoapControlClient client = new CoapControlClient("coap://127.0.0.1:5683");
-        CoapControlServer.getInstance().start();
+        if (System.getenv(Constants.DEVICE_CONFIG_FILE) != null) {
+            String deviceFile = System.getenv(Constants.DEVICE_CONFIG_FILE);
+            Device device = JSONSerializer.deserializeDevice(new File(deviceFile));
+            device.registerToCoapServer();
+            CoapControlServer.getInstance().start();
+        }
 
-
-
-
-
+        if (System.getenv(Constants.ACTIVE_CONFIG_FILE) != null) {
+            String file = System.getenv(Constants.ACTIVE_CONFIG_FILE);
+            ActiveDevice activeDevice = JSONSerializer.deserializeActiveDevice(new File(file));
+            activeDevice.getDevice().registerToCoapServer();
+            activeDevice.start();
+        }
     }
 }
--- src/main/java/io/patriot_framework/generator/runner/configuration/Constants.java
+++ src/main/java/io/patriot_framework/generator/runner/configuration/Constants.java
@@ -1,7 +1,7 @@
 package io.patriot_framework.generator.runner.configuration;
 
 /**
- * @author <a href="mailto:jakub.smadis@gmail.com">Jakub Smadiš</a>
+ * @author <a href="mailto:jakub.smadis@gmail.com">Jakub Smadis</a>
  */
 public class Constants {
     public static final String DEVICE_CONFIG_FILE = "PATRIOT_DATA_GENERATOR_DEVICE_FILE";
